005dd6d8748dd1a6e35cacf76de4af8a7676e616
Yekna <urosjeknic@gmail.com>
2024-01-16 20:54:58 +0100

    Created Hero Layout

---
+++ src/components/Hero.tsx
@@ -0,0 +1,28 @@
+import type { FC, ReactNode } from "react";
+
+type Props = {
+  src: string;
+  maxWidth: number;
+  color: string;
+  children: ReactNode;
+};
+
+const Hero: FC<Props> = ({ src, maxWidth, color, children }) => {
+  return (
+    <section
+      className="p-10 min-h-[720px] grid place-content-center"
+      style={{
+        background: `url(${src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color,
+      }}
+    >
+      <div className="grid gap-3" style={{ maxWidth: maxWidth + "px" }}>
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
