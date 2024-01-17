ce6c1bf6cad8b54346a6823cff899339ed20c218
Yekna <urosjeknic@gmail.com>
2024-01-17 21:20:42 +0100

    Add title prop to text columns

--- src/components/TextText.tsx
+++ src/components/TextText.tsx
@@ -3,12 +3,16 @@ import type { FC, ReactNode } from "react";
 type Props = {
     children: ReactNode;
     columns: number;
+    title: string;
 };
 
-const TextText: FC<Props> = ({ children, columns }) => {
+const TextText: FC<Props> = ({ children, columns, title }) => {
     return (
-        <section className="grid gap-5" style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}>
-            {children}
+        <section className="grid gap-5" >
+            <h1 className="text-center text-2xl">{title}</h1>
+            <div className="grid gap-5" style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}>
+                {children}
+            </div>
         </section>
     );
 };
